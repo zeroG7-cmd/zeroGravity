be1c0f2a840753932fa010217b25ebcf83f040b3
zeroG7-cmd <zerogravity845@gmail.com>
2026-06-16 22:03:00 +0100

    Create zer0Gravity_data_pipeline.py

---
+++ scripts/zer0Gravity_data_pipeline.py
@@ -0,0 +1,29 @@
+#-------------------#
+#Import             #
+#--------------------#
+import os
+import sqlite3
+import panda
+
+#
+# Path File/Configuration #
+#
+
+
+#
+#Runtime Arguments/ Input #
+#
+
+
+
+#
+# create table Function
+
+
+
+#Export Function 
+
+
+
+
+#Main Runner
